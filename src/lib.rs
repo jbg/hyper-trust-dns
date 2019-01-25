ec039377eb3ecab376bfc80694b0541b27a05f91
Jasper Bryant-Greene <jbg@rf.net.nz>
2019-01-25 11:41:13 +0800

    derive Clone for HyperTrustDnsResolver and mark the returned future as Send

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use hyper::rt::Future;
 use trust_dns_resolver::AsyncResolver;
 
 
+#[derive(Clone)]
 pub struct HyperTrustDnsResolver(AsyncResolver);
 
 impl HyperTrustDnsResolver {
@@ -19,7 +20,7 @@ impl HyperTrustDnsResolver {
 
 impl Resolve for HyperTrustDnsResolver {
     type Addrs = vec::IntoIter<IpAddr>;
-    type Future = Box<Future<Item=Self::Addrs, Error=Error>>;
+    type Future = Box<Future<Item=Self::Addrs, Error=Error> + Send>;
 
     fn resolve(&self, name: Name) -> Self::Future {
         Box::new(
