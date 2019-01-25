5c8366ec56055630613a13cfe6baab1c5f9669d2
Jasper Bryant-Greene <jbg@rf.net.nz>
2019-01-25 11:37:40 +0800

    very basic integration of hyper and trust-dns-resolver

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+#![feature(self_struct_ctor)]
+
+use std::io::Error;
+use std::net::IpAddr;
+use std::vec;
+
+use hyper::client::connect::dns::{Name, Resolve};
+use hyper::rt::Future;
+use trust_dns_resolver::AsyncResolver;
+
+
+pub struct HyperTrustDnsResolver(AsyncResolver);
+
+impl HyperTrustDnsResolver {
+    pub fn new(resolver: AsyncResolver) -> Self {
+        Self(resolver)
+    }
+}
+
+impl Resolve for HyperTrustDnsResolver {
+    type Addrs = vec::IntoIter<IpAddr>;
+    type Future = Box<Future<Item=Self::Addrs, Error=Error>>;
+
+    fn resolve(&self, name: Name) -> Self::Future {
+        Box::new(
+            self.0
+                .lookup_ip(name.as_str())
+                .map(|res|
+                    res.iter()
+                       .collect::<Vec<_>>()
+                       .into_iter()
+                )
+                .map_err(|e| e.into())
+        )
+    }
+}
